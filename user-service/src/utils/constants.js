81494f18ef3008119c3bedaf3bc9c0f33e86ce39
AshishBiswas-2027 <ab23ceb0b07@student.nitw.ac.in>
2026-06-20 11:06:48 +0530

    feat(notification-service): initialize notification service with basic structure and dependencies
    - Added package.json with necessary dependencies and scripts
    - Created configuration files for Kafka and logger
    - Implemented main entry point (index.js) for the service
    - Set up email consumer for processing email notifications
    - Developed email service for handling email-related functionalities
    - Established template structure for email content
    - Defined constants for the service
    - Added Kafka configuration for user service producer
    - Created notification producer for user service
    - Defined constants for user service

---
+++ user-service/src/utils/constants.js
@@ -0,0 +1,11 @@
+//must match notification service
+
+
+const TOPICS = {
+  OTP_EMAIL: 'notification.email.otp',
+  WELCOME_EMAIL: 'notification.email.welcome',
+  TICKET_CONFIRMATION: 'notification.email.ticket-confirmation',
+  PASSWORD_RESET: 'notification.email.password-reset',
+};
+
+module.exports = {TOPICS};
